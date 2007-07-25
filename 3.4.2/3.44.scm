efb593d1ee5d956bc3678fabefcdba2e6316a380
Peter Danenberg <pcd@wikitex.org>
2007-07-25 16:15:11 +0000

    preservation of balance

---
+++ 3.4.2/3.44.scm
@@ -0,0 +1,7 @@
+;; The difficulty with exchange happened at the diffirence step:
+;; subtracting one account's balance from the other was not
+;; synchronized with deposit and withdrawal, resulting in
+;; irregularities.
+
+;; With transfer, however, there is no anatomic middle step; and
+;; balance is automatically preserved.
