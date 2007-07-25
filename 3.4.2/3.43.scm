fb5d809f77470a8a299e9a7e5e47bc202fe35cc5
Peter Danenberg <pcd@wikitex.org>
2007-07-25 16:02:36 +0000

    pathological concurrency

---
+++ 3.4.2/3.43.scm
@@ -0,0 +1,16 @@
+;; Through composition of serializers, any two accounts are locked in
+;; mutual read-write access; impervious to third-party interference
+;; (in accordance with the mutex-contract).
+
+;; Since exchange is commutative and conservative, dollar amount are
+;; preserved across transactions; what results is some permutation of
+;; the original set.
+
+;; See 3.43a.png.
+
+;; Sum of balances will be preserved, since reading and transacting
+;; are atomic: fixed deltas are transmitted through time; otherwise,
+;; the reading of a balance which mutated between said reading and
+;; subtraction can result in loss and creation of money.
+
+;; See 3.43b.png.
