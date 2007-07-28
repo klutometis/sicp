e496dbba50445b0c8ba14cc3e100f78695895175
Peter Danenberg <pcd@wikitex.org>
2007-07-28 11:13:37 +0000

    RLC circuit

---
+++ 3.5.4/RLC.scm
@@ -0,0 +1,12 @@
+(load "integral.scm")
+(load "add-streams.scm")
+(load "scale-stream.scm")
+
+(define (RLC R L C dt)
+  (lambda (vc0 il0)
+    (define il (integral (delay dil) il0 dt))
+    (define vc (integral (delay dvc) vc0 dt))
+    (define dil (add-streams (scale-stream vc (/ 1 C))
+                            (scale-stream il (- (/ R l)))))
+    (define dvc (scale-stream il (- (/ 1 C))))
+    (cons il vc)))
