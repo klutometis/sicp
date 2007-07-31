4d0db2821bf45ea7102ca1c858a6f157711e1e34
Peter Danenberg <pcd@wikitex.org>
2007-07-31 07:01:44 +0000

    native vs. primitive map

---
+++ 4.1.4/4.14.scm
@@ -0,0 +1,5 @@
+;; Primitive map tries to execute the metacircular-evaluator's
+;; interpretation of lambda (a list prefaced by 'procedure); which
+;; worketh nought.
+
+;; Map must know how to evaluate the m-e's procedures.
