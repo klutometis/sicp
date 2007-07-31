36f68e45fa21b10a85b1aa221f4fc3014c3d8d1a
Peter Danenberg <pcd@wikitex.org>
2007-07-31 16:00:24 +0000

    contradiction

--- 4.1.5/4.15.scm
+++ 4.1.5/4.15.scm
@@ -1,4 +1,4 @@
 ;; Let H(P, I) be that P halts on input I, and K(P) <-> ~H(P, P).
 
-;; K(K) <-> ~H(K, K); but K -> ~H(K, K) -> ~K(K) and
-;; ~K -> H(K, K) -> K(K), which violates H's contract.
+;; Since K(K) <-> ~H(K, K), however, K -> ~K and ~K -> K; which is a
+;; contradiction.
