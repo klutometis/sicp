8f349861c25edfa4f958f914efe198a90e6f2538
Peter Danenberg <pcd@wikitex.org>
2007-08-01 01:23:51 +0000

    clarify correct programs' immunity

--- 4.1.6/4.17.scm
+++ 4.1.6/4.17.scm
@@ -1,7 +1,8 @@
 ;; The extraneous frame owes to the stacked lambda to which let
-;; decomposes.  A correct program will have sequential definitions,
-;; anyway.  Define the lambdas directly in the parent frame without
-;; recourse to let to implement simultaneous scope.
+;; decomposes.  A correct program will have not attempt to evaluate
+;; internal definitions before the are define, anyway.  Define the
+;; lambdas directly in the parent frame without recourse to let to
+;; implement simultaneous scope.
 
 ;; See 4.17.png.
 
