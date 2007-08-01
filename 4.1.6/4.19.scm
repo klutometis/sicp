a3b5be2922c1b003b3968427bebd2099beffcf10
Peter Danenberg <pcd@wikitex.org>
2007-08-01 01:36:51 +0000

    lazy evaluation?

---
+++ 4.1.6/4.19.scm
@@ -0,0 +1,4 @@
+;; The program should fail loudly and quickly rather than extrapolate
+;; upon error; on the other hand, Eva's plan might be implemented by
+;; lazy evaluation: promise each internal definition, and evaluate by
+;; need.
