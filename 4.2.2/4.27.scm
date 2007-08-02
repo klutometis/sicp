81861be238d3305290949cf00af5fa571b4c20fd
Peter Danenberg <pcd@wikitex.org>
2007-08-02 00:10:20 +0000

    refine lazy eval + side-effects with test

--- 4.2.2/4.27.scm
+++ 4.2.2/4.27.scm
@@ -1,17 +1,30 @@
+(load-option 'regular-expression)
+(load-option 'format)
+
+(load "test.scm")
 (load "eval-global.scm")
 (load "install-packages.scm")
 
 (install-packages)
 
-(eval-global '(define count 0))
-(eval-global '(define (id x)
-                (set! count (+ count 1))
-                x))
+(define expressions
+  (list
+   '(define count 0)
+   '(define (id x)
+      (set! count (+ count 1))
+      x)
+   '(define w (id (id 10)))
+   'count
+   'w
+   'count))
 
-(eval-global '(define w (id (id 10))))
-(eval-global 'count)
-;; 1
-(eval-global 'w)
-;; (thunk (id 10) (#[hash-table 70]))
-(eval-global 'count)
-;; 1
+(define lazy-side-effects
+  (fold-right cons '() (map eval-global expressions)))
+
+(test
+ "lazy evaluation and side effects"
+ "(ok ok ok 1 (thunk (id 10) (#.hash-table [0-9]*.)) 1)"
+ lazy-side-effects
+ 're-string-match
+ (lambda (expected got)
+   (not (false? (re-string-match expected (format #f "~A" got))))))
