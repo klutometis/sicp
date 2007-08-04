931826c9025836ee09100039febd7124aad1f84c
Peter Danenberg <pcd@wikitex.org>
2007-08-04 07:25:47 +0000

    rearrangement

---
+++ 4.3.2/4.39.scm
@@ -0,0 +1,46 @@
+(load "test.scm")
+(load "install-packages.scm")
+(load "ambeval-global.scm")
+(load "ambeval-global-until-fail.scm")
+(load "require.scm")
+(load "distinct?.scm")
+(load "multiple-dwelling.scm")
+(load "multiple-dwelling-rearranged.scm")
+
+(install-packages)
+(ambeval-global require)
+(ambeval-global distinct?)
+(ambeval-global multiple-dwelling)
+(ambeval-global multiple-dwelling-rearranged)
+
+(define original-time
+  (let ((time 0))
+    (with-timings
+     (lambda ()
+       (ambeval-global-until-fail '(multiple-dwelling)))
+     (lambda (run gc real)
+       (set! time real)))
+    time))
+
+(define rearranged-time
+  (let ((time 0))
+    (with-timings
+     (lambda ()
+       (ambeval-global-until-fail '(multiple-dwelling-rearranged)))
+     (lambda (run gc real)
+       (set! time real)))
+    time))
+
+(test
+ "original and rearranged time comparable within [0.75, 1.25]"
+ original-time
+ rearranged-time
+ "0.75 <= original-time / rearranged-time <= 1.25"
+ (lambda (original rearranged)
+   (let ((a 0.75)
+         (b 1.25))
+     (< (/ a b) (/ original (* rearranged b))))))
+
+;; No: each of the conditions is equi-determinant upon the others; in
+;; order to pare down the tree, we need a hierarchy of
+;; specific-to-less-specific constraints.
--- 4.3.2/multiple-dwelling-rearranged.scm
+++ 4.3.2/multiple-dwelling-rearranged.scm
@@ -1,10 +1,12 @@
-(define multiple-dwelling
-  '(define (multiple-dwelling)
+(define multiple-dwelling-rearranged
+  '(define (multiple-dwelling-rearranged)
      (let ((baker (amb 1 2 3 4 5))
            (cooper (amb 1 2 3 4 5))
            (fletcher (amb 1 2 3 4 5))
            (miller (amb 1 2 3 4 5))
            (smith (amb 1 2 3 4 5)))
+       (require
+        (distinct? (list baker cooper fletcher miller smith)))
        (require (not (= baker 5)))
        (require (not (= cooper 1)))
        (require (not (= fletcher 5)))
@@ -12,8 +14,6 @@
        (require (> miller cooper))
        (require (not (= (abs (- smith fletcher)) 1)))
        (require (not (= (abs (- fletcher cooper)) 1)))
-       (require
-        (distinct? (list baker cooper fletcher miller smith)))
        (list (list 'baker baker)
              (list 'cooper cooper)
              (list 'fletcher fletcher)
