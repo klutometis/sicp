302e2e0f91f6584c4648472860518cbd8f68ab7b
Peter Danenberg <pcd@wikitex.org>
2007-08-07 04:06:13 +0000

    back to big shots sans warbucks

--- 4.4.1/4.58.scm
+++ 4.4.1/4.58.scm
@@ -6,37 +6,15 @@
               (and (job ?p1 (?div . ?r1))
                    (job ?p2 (?div . ?r2)))))
 
-;; ATTN: hideous hack! Contrary to the spirit (and letter) of logical
-;; programming.
-(define unique-values '())
-(query '(rule (unique ?p)
-              (and (always-true)
-                   (lisp-value
-                    (lambda (p) (if (member p unique-values)
-                                    #f
-                                    (let ((pair (list p)))
-                                      (if (null? unique-values)
-                                          (set! unique-values pair)
-                                          (begin
-                                            (set-cdr! unique-values (list p))
-                                            #t))))) ?p))))
-(query '(rule (no-supervisors ?q)
-              (and (supervisor ?p ?q)
-                   (not (supervisor ?q ?r)))))
 (query '(rule (big-shot ?p)
-              (and (or (no-supervisors ?p)
-                       (and (supervisor ?p ?s2)
-                            (not (same-division ?p ?s2))))
-                   (unique ?p))))
+              (and (supervisor ?p ?s2)
+                   (not (same-division ?p ?s2)))))
 
-;; Without the no-supervisors rule, big-shot doesn't include those
-;; with no supervisors; no-supervisors introduces some complexity,
-;; however.  (Can we resolve the spurious duplicates without recourse
-;; to state-hacks?)
+;; NB: does not contain Warbucks, who has no supervisors!
 (define big-shots (query '(big-shot ?p)))
 
 (test
- "big shots (NB: assumes that big-shot is a supervisor of somebody)"
- '((big-shot (warbucks oliver)) (big-shot (scrooge eben)) (big-shot (bitdiddle ben)))
+ "big shots sans warbucks"
+ '((big-shot (scrooge eben)) (big-shot (bitdiddle ben)))
  big-shots
  'equal? equal?)
