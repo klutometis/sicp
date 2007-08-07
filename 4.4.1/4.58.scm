1e6c11121242c88f08295fa63445afbcbda0f09b
Peter Danenberg <pcd@wikitex.org>
2007-08-07 02:39:10 +0000

    add spurious warbucks'

--- 4.4.1/4.58.scm
+++ 4.4.1/4.58.scm
@@ -5,19 +5,18 @@
 (query '(rule (same-division ?p1 ?p2)
               (and (job ?p1 (?div . ?r1))
                    (job ?p2 (?div . ?r2)))))
-;; (query '(rule (no-supervisors ?q)
-;;               (and (supervisor ?p ?q)
-;;                    (not (supervisor ?q ?r)))))
+(query '(rule (no-supervisors ?q)
+              (and (supervisor ?p ?q)
+                   (not (supervisor ?q ?r)))))
 (query '(rule (big-shot ?p)
               (or (no-supervisors ?p)
                   (and (supervisor ?p ?s2)
                        (not (same-division ?p ?s2))))))
-;; Doesn't include warbucks; who has no supervisor!  (Got spurious
-;; repetitions from (no-supervisors).)
-(define big-shots-sans-warbucks (query '(big-shot ?p)))
+;; Includes spurious Warbucks'.
+(define big-shots (query '(big-shot ?p)))
 
 (test
- "big shots sans warbucks"
- '((big-shot (scrooge eben)) (big-shot (bitdiddle ben)))
- big-shots-sans-warbucks
+ "big shots with spurious warbucks'"
+ '((big-shot (warbucks oliver)) (big-shot (scrooge eben)) (big-shot (warbucks oliver)) (big-shot (bitdiddle ben)) (big-shot (warbucks oliver)))
+ big-shots
  'equal? equal?)
