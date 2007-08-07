c5d1c258264e875a2df41eb8600d3e12cc7b2b66
Peter Danenberg <pcd@wikitex.org>
2007-08-07 19:47:39 +0000

    big-shots with redundant warbucks'

--- 4.4.1/4.58.scm
+++ 4.4.1/4.58.scm
@@ -6,15 +6,21 @@
                        (and (job ?p1 (?div . ?r1))
                             (job ?p2 (?div . ?r2))))))
 
-(query '(assert! (rule (big-shot ?p)
-                       (and (supervisor ?p ?s2)
-                            (not (same-division ?p ?s2))))))
+(query '(assert! (rule (only-sub-supervisors ?q)
+                       (and (supervisor ?p ?q)
+                            (not (supervisor ?q ?r))))))
 
-;; NB: does not contain Warbucks, who has no supervisors!
+(query '(assert! (rule (big-shot ?p)
+                       (or (only-sub-supervisors ?p)
+                           (and (supervisor ?p ?s2)
+                                (not (same-division ?p ?s2)))))))
+
+;; NB: big shots must supervise someone; results contain redundant
+;; Warbucks'.
 (define big-shots (query '(big-shot ?p)))
 
 (test
- "big shots sans warbucks"
- '((big-shot (scrooge eben)) (big-shot (bitdiddle ben)))
+ "big shots with redundant warbucks"
+ '((big-shot (warbucks oliver)) (big-shot (scrooge eben)) (big-shot (warbucks oliver)) (big-shot (bitdiddle ben)) (big-shot (warbucks oliver)))
  big-shots
  'equal? equal?)
