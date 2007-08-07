929cc82c0ea2128ee092a51c6349e3a5c4c3f930
Peter Danenberg <pcd@wikitex.org>
2007-08-07 20:50:44 +0000

    history mechanism outline

---
+++ 4.4.3/4.67.scm
@@ -0,0 +1,4 @@
+;; Before the system unifies a pattern with a frame, it should search
+;; the history of pattern-frame pairs and abandon the query if it's
+;; found there; the history should be modified after the decision to
+;; apply is made.
