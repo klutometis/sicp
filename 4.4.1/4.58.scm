de30496e8b4fd7ca8370f171ff2e383582a0fd33
Peter Danenberg <pcd@wikitex.org>
2007-08-07 04:01:43 +0000

    unique hack; ugly!

--- 4.4.1/4.58.scm
+++ 4.4.1/4.58.scm
@@ -5,18 +5,38 @@
 (query '(rule (same-division ?p1 ?p2)
               (and (job ?p1 (?div . ?r1))
                    (job ?p2 (?div . ?r2)))))
+
+;; ATTN: hideous hack! Contrary to the spirit (and letter) of logical
+;; programming.
+(define unique-values '())
+(query '(rule (unique ?p)
+              (and (always-true)
+                   (lisp-value
+                    (lambda (p) (if (member p unique-values)
+                                    #f
+                                    (let ((pair (list p)))
+                                      (if (null? unique-values)
+                                          (set! unique-values pair)
+                                          (begin
+                                            (set-cdr! unique-values (list p))
+                                            #t))))) ?p))))
 (query '(rule (no-supervisors ?q)
               (and (supervisor ?p ?q)
                    (not (supervisor ?q ?r)))))
 (query '(rule (big-shot ?p)
-              (or (no-supervisors ?p)
-                  (and (supervisor ?p ?s2)
-                       (not (same-division ?p ?s2))))))
-;; Includes spurious Warbucks'.
+              (and (or (no-supervisors ?p)
+                       (and (supervisor ?p ?s2)
+                            (not (same-division ?p ?s2))))
+                   (unique ?p))))
+
+;; Without the no-supervisors rule, big-shot doesn't include those
+;; with no supervisors; no-supervisors introduces some complexity,
+;; however.  (Can we resolve the spurious duplicates without recourse
+;; to state-hacks?)
 (define big-shots (query '(big-shot ?p)))
 
 (test
- "big shots with spurious warbucks'"
- '((big-shot (warbucks oliver)) (big-shot (scrooge eben)) (big-shot (warbucks oliver)) (big-shot (bitdiddle ben)) (big-shot (warbucks oliver)))
+ "big shots (NB: assumes that big-shot is a supervisor of somebody)"
+ '((big-shot (warbucks oliver)) (big-shot (scrooge eben)) (big-shot (bitdiddle ben)))
  big-shots
  'equal? equal?)
