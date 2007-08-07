ed861ea8e0fa99febd78a1a16c7e1f509dacc19f
Peter Danenberg <pcd@wikitex.org>
2007-08-07 21:24:22 +0000

    test-based reversal

--- 2.2.1/2.18.scm
+++ 2.2.1/2.18.scm
@@ -1,5 +1,18 @@
+(load "test.scm")
 (load "reverse.scm")
 
-(define l '(1 2 3 4))
-(reverse l)
-(equal? l (reverse (reverse l)))
+(define orthogonal '(1 2 3 4))
+(define reversal (reverse orthogonal))
+(define reverse-reversal (reverse reversal))
+
+(test
+ "reversal"
+ '(4 3 2 1)
+ reversal
+ 'equal? equal?)
+
+(test
+ "reversal of the reversal"
+ orthogonal
+ reverse-reversal
+ 'equal? equal?)
