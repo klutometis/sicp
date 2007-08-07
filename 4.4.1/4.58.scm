bcba96c4d94164dd08b4d4ed4385674c519d28f9
Peter Danenberg <pcd@wikitex.org>
2007-08-07 02:29:12 +0000

    big-shot sans spurious repetitions

---
+++ 4.4.1/4.58.scm
@@ -0,0 +1,17 @@
+(load "context.scm")
+
+(initialize-data-base microshaft-data-base)
+
+(query '(rule (same-division ?p1 ?p2)
+              (and (job ?p1 (?div . ?r1))
+                   (job ?p2 (?div . ?r2)))))
+;; (query '(rule (no-supervisors ?q)
+;;               (and (supervisor ?p ?q)
+;;                    (not (supervisor ?q ?r)))))
+(query '(rule (big-shot ?p)
+              (or (no-supervisors ?p)
+                  (and (supervisor ?p ?s2)
+                       (not (same-division ?p ?s2))))))
+;; Doesn't include warbucks; who has no supervisor!  (Got spurious
+;; repetitions from (no-supervisors).)
+(query '(big-shot ?p))
