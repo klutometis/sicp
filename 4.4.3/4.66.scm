49ab00f4ab8a270c6f41bf9a108a73105b5a8cf1
Peter Danenberg <pcd@wikitex.org>
2007-08-07 20:28:51 +0000

    redundant frame problem

---
+++ 4.4.3/4.66.scm
@@ -0,0 +1,5 @@
+;; The accumulation scheme would produce spurious results in those
+;; cases with redundant frames; one could implement a special form
+;; "unique" which prunes the redundant frames, and accumulate
+;; thereupon.  (Are there deductive difficulties, such as cases where
+;; non-uniqueness is desirable?)
