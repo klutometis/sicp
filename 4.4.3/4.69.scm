b813d4b263a6d2a4bfe60bd11de2c7f746b56197
Peter Danenberg <pcd@wikitex.org>
2007-08-08 21:12:14 +0000

    great relations

---
+++ 4.4.3/4.69.scm
@@ -0,0 +1,29 @@
+(load "context.scm")
+(load "last-pair.scm")
+(load "genealogy.scm")
+
+(initialize-data-base '())
+(install last-pair)
+(install genealogy)
+
+(define great-grandsons (query '((great great grandson) ?x ?y)))
+(define fives-great-grandsons (query '((great great great great great grandson) ?x ?y)))
+(define adam-irad (query '(?relationship adam irad)))
+
+(test
+ "great grandsons (some redundance)"
+ '(((great great grandson) irad jubal) ((great great grandson) enoch lamech) ((great great grandson) irad jabal) ((great great grandson) cain methushael) ((great great grandson) adam mehujael))
+ great-grandsons
+ 'equal? equal?)
+
+(test
+ "five-times great grandsons"
+ '(((great great great great great grandson) adam jubal) ((great great great great great grandson) adam jabal))
+ fives-great-grandsons
+ 'equal? equal?)
+
+(test
+ "relationship between adam and irad"
+ '(((great great . son) adam irad) ((great grandson) adam irad))
+ adam-irad
+ 'equal? equal?)
