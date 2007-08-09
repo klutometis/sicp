93aaad4f830a37a9ecd99120f82e18ca96258727
Peter Danenberg <pcd@wikitex.org>
2007-08-09 00:02:54 +0000

    clarify reverse

--- 4.4.3/reverse-intermediary.scm
+++ 4.4.3/reverse-intermediary.scm
@@ -2,14 +2,14 @@
 
 ;; Snarfed from James Power's Prolog Tutorial:
 ;; http://www.cs.nuim.ie/~jpower/Courses/PROLOG/list-acc.html
-(define reverse-intermediary
+(define reverse-accumulate
   '((assert! (rule (reverse ?list ?reversed)
                    ;; Call the three-argument version with accumulator
                    (reverse ?list () ?reversed)))
     ;; When the first list is exhausted, you're left with the
-    ;; interversed; which happens to be the reversed list.
+    ;; accumulated list; which happens to be the reversed list.
     (assert! (rule (reverse () ?reversed ?reversed)))
-    ;; Successively move ?heads to the ?interversed, leaving ?reversed
+    ;; Successively move ?heads to the ?accumulated, leaving ?reversed
     ;; alone until the second rule.
-    (assert! (rule (reverse (?head . ?tail) ?interversed ?reversed)
-                   (reverse ?tail (?head . ?interversed) ?reversed)))))
+    (assert! (rule (reverse (?head . ?tail) ?accumulated ?reversed)
+                   (reverse ?tail (?head . ?accumulated) ?reversed)))))
