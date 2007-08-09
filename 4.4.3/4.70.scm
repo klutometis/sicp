e93f3e9c81e679cb69f25c3931fb351aa7e98567
Peter Danenberg <pcd@wikitex.org>
2007-08-09 01:41:08 +0000

    indirection

---
+++ 4.4.3/4.70.scm
@@ -0,0 +1,31 @@
+(load "context.scm")
+(load "genealogy.scm")
+
+(initialize-data-base '())
+
+(install genealogy)
+
+(define finishes (query '((great great great great great grandson) ?x ?y)))
+
+(define (add-assertion! assertion)
+  (store-assertion-in-index assertion)
+  (set! THE-ASSERTIONS
+        (cons-stream assertion THE-ASSERTIONS))
+  'ok)
+
+(install genealogy)
+
+(test
+ "query finishes before redefinition of add-assertion!"
+ '(((great great great great great grandson) adam jubal) ((great great great great great grandson) adam jabal))
+ finishes
+ 'equal? equal?)
+
+(try-kill
+ (lambda () (query '((great great great great great grandson) ?x ?y)))
+ "infinite loop after redefinition of add-assertion!"
+ 1000)
+
+;; (let ...) creates an environment in which to delay the processing
+;; of THE-ASSERTIONS until cons-stream can successfully set up a
+;; promise; it does so by way of ((lambda () <body>) THE-ASSERTIONS).
