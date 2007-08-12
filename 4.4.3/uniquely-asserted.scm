d7fe79fcb98a28783f2cc2b4574bddd50d9b9c4a
Peter Danenberg <pcd@wikitex.org>
2007-08-12 04:37:38 +0000

    sans debugs

--- 4.4.3/uniquely-asserted.scm
+++ 4.4.3/uniquely-asserted.scm
@@ -1,12 +1,8 @@
 (define (uniquely-asserted contents frame-stream)
-;  (write-line (stream->list frame-stream))
   (stream-flatmap
    (lambda (frame)
-;     (write-line (stream->list (qeval (car contents) (singleton-stream frame))))
      (let ((qeval-stream (qeval (car contents) (singleton-stream frame))))
-;       (write-line (stream->list qeval-stream))
-;       (write-line qeval-stream)
        (if (= 1 (stream-length qeval-stream))
            qeval-stream
            the-empty-stream)))
-     frame-stream))
+   frame-stream))
