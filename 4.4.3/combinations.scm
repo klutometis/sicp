ceff9f1f6cf7db266d28f339fd8b83086cd1c862
Peter Danenberg <pcd@wikitex.org>
2007-08-13 01:50:43 +0000

    last-gasp for scheme-in-scheme

--- 4.4.3/combinations.scm
+++ 4.4.3/combinations.scm
@@ -25,3 +25,7 @@
               ((lambda ,variables ,(nested-map combinanda))
                      ,@(quote-variables combinanda)) combinations)))
       (eval evaluandum (nearest-repl/environment)))))
+
+(combinations '(1 2) '(3 4 5))
+
+;; ((1 3) (1 4) (1 5) (2 3) (2 4) (2 5))
