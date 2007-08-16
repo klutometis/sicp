f704326c9938c820cf0dafe00bd441ba4586208a
Peter Danenberg <pcd@wikitex.org>
2007-08-16 01:34:11 +0000

    cruft

--- 4.4.3/compile.scm
+++
@@ -1,8 +0,0 @@
-(let ((table (make-equal-hash-table)))
-  (hash-table/put! table '(nigs) '(nogs))
-  (hash-table/put! table '(rigs) '(sogs))
-  (hash-table/put! table '(nigs) '(noogs))
-  (hash-table->alist table))
-
-(define alist (((? middle-manager) aull dewitt) ((? middle-manager) warbucks oliver) ((? x) aull dewitt)))
-
