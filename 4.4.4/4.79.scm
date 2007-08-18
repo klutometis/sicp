cc4590a0929661fab1264b3f1ab35e51c0ecd69a
Peter Danenberg <pcd@wikitex.org>
2007-08-18 04:55:50 +0000

    clarification of environmental model

--- 4.4.4/4.79.scm
+++ 4.4.4/4.79.scm
@@ -1,11 +1,12 @@
+(load "query-environment-context.scm")
+
+(initialize-data-base microshaft-data-base)
+
 (define (non-rule-query)
   (query '(job ?x ?y)))
 
 (define (rule-query)
   (query '(lives-near ?x ?y)))
-
-(load "query-environment-context.scm")
-(initialize-data-base microshaft-data-base)
 
 (define environment-non-rule-query
   (non-rule-query))
@@ -14,6 +15,7 @@
   (rule-query))
 
 (load "query-context.scm")
+
 (initialize-data-base microshaft-data-base)
 
 (define frame-non-rule-query
@@ -34,3 +36,21 @@
  environment-rule-query
  'equal? equal?)
 
+;; See:
+;; 
+;; query-environment/apply-a-rule.scm
+;; query-environment/apply-rules.scm
+;; query-environment/check-an-assertion.scm
+;; query-environment/conjoin.scm query-environment/depends-on?.scm
+;; environment-bound-symbol?.scm environment-lookup-symbol.scm
+;; query-environment/extend-if-consistent.scm
+;; query-environment/extend-if-possible.scm
+;; query-environment/extend.scm query-environment/fetch-assertions.scm
+;; query-environment/fetch-rules.scm
+;; query-environment/find-assertions.scm
+;; query-environment/instantiate.scm make-environment.scm
+;; query-environment/pattern-match.scm query-environment/qeval.scm
+;; query-environment/query.scm query-environment/simple-query.scm
+;; query-environment/unify-match.scm variable-symbol.scm
+;; 
+;; for changes.
