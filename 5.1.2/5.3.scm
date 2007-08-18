f36a16e189b04eeda8a74d37e32f80d49366a38e
Peter Danenberg <pcd@wikitex.org>
2007-08-18 11:03:18 +0000

    sqrt-machines: primitive and arithmetic

---
+++ 5.1.2/5.3.scm
@@ -0,0 +1,38 @@
+(load "register-machine-context.scm")
+(load "test-context.scm")
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(load "register-machine/sqrt-machine-primitive.scm")
+
+(set-register-contents! sqrt-machine-primitive 'x 2.0)
+(start sqrt-machine-primitive)
+(define sqrt-primitive-2.0
+  (get-register-contents sqrt-machine-primitive 'guess))
+
+(load "register-machine/sqrt-machine-arithmetic.scm")
+
+(set-register-contents! sqrt-machine-arithmetic 'x 2.0)
+(start sqrt-machine-arithmetic)
+(define sqrt-arithmetic-2.0
+  (get-register-contents sqrt-machine-arithmetic 'guess))
+
+(test
+ "primitive produces correct result to four decimals"
+ 1.4142
+ sqrt-primitive-2.0
+ '(tolerant= x y 0.0001)
+ (lambda (x y) (tolerant= x y 0.0001)))
+
+(test
+ "primitive and arithmetic machines produce the same result"
+ sqrt-primitive-2.0
+ sqrt-arithmetic-2.0
+ '= =)
