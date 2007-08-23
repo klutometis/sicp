4bd8e912a314a6b7354d6a689c559b58d559ac26
Peter Danenberg <pcd@wikitex.org>
2007-08-23 09:16:52 +0000

    recursive and iterative count-leaves

---
+++ 5.3.1/5.21.scm
@@ -0,0 +1,28 @@
+(load "register-machine-trace-label-context.scm")
+(load "test-context.scm")
+(load "register-machine/count-leaves-recursive.scm")
+(load "register-machine/count-leaves-iterative.scm")
+
+(define tree '((1 2) 3 4 (5 6 7 (2 3))))
+
+(set-register-contents! count-leaves-recursive 'tree tree)
+(start count-leaves-recursive)
+(define recursive
+  (get-register-contents count-leaves-recursive 'val))
+
+(set-register-contents! count-leaves-iterative 'tree tree)
+(start count-leaves-iterative)
+(define iterative
+  (get-register-contents count-leaves-iterative 'val))
+
+(test
+ "recursive-count is correct"
+ 9
+ recursive
+ '= =)
+
+(test
+ "recursive and iterative issue identically"
+ recursive
+ iterative
+ '= =)
