8e48175400e959f8f080b6e397498d889066a658
Peter Danenberg <pcd@wikitex.org>
2007-08-25 09:02:00 +0000

    test for basic cond

--- 5.4.3/5.24.scm
+++ 5.4.3/5.24.scm
@@ -1,8 +1,16 @@
+(load "test-context.scm")
 (load "eceval-context.scm")
 ;;; See ev-cond-basic.scm for monkey-patch.
 (load "eceval/ev-cond-basic.scm")
 (load "eceval/eceval.scm")
 
 (define the-global-environment (setup-environment))
-(eceval-global
- '(cond (false 1) ((= 1 2) 2) (else (+ 1 2))))
+(define cond-result
+  (eceval-global
+   '(cond (false 1) ((= 1 2) 2) (else (+ 1 2)))))
+
+(test
+ "cond as basic form"
+ 3
+ cond-result
+ '= =)
