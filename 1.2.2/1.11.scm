97a995bc646929e4d8b69d64a09a6dd8134932b4
Peter Danenberg <pcd@wikitex.org>
2008-02-26 15:36:35 +0000

    should use testing framework; duncanm, case

--- 1.2.2/1.11.scm
+++ 1.2.2/1.11.scm
@@ -1,25 +1,26 @@
 ;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
 ;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
 
-(define (f n)
+;;; Using an idiosyncratic chicken package here; not compatible with
+;;; MIT-scheme.
+(require-extension check)
+
+(define (f-rec n)
   (cond ((< n 3) n)
-        (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3)))))))
+        (else (+ (f-rec (- n 1)) (* 2 (f-rec (- n 2)))
+                 (* 3 (f-rec (- n 3)))))))
 
-(define (f n)
+;;; Thanks, duncanm, for the suggestion to use case.
+(define (f-iter n)
   (define (iter x y z count)
-    (display (string-append (number->string x) " " (number->string y) " " (number->string z)))
-    (newline)
-    (cond ((= count 0) x)
-          ((= count 1) y)
-          ((= count 2) z)
- (else (iter y z (+ (* 3 x) (* 2 y) z) (- count 1)))))
-  (iter 0 1 2 n)
-  )
+    (case count
+      ((0) x)
+      ((1) y)
+      ((2) z)
+      (else (iter y z (+ (* 3 x) (* 2 y) z) (- count 1)))))
+  (iter 0 1 2 n))
 
-
-(f 0)
-(f 1)
-(f 2)
-(f 3)
-(f 4)
-(f 5)
+(define n 5)
+(define n-rec (f-rec 5))
+(define n-iter (f-iter 5))
+(check n-rec => n-iter)
