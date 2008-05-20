6540d03ea052d6d9c89f99589c791a3cbecf943c
Peter Danenberg <pcd@wikitex.org>
2008-05-20 07:27:11 -0700

    checks

--- 1.2.1/1.10.scm
+++ 1.2.1/1.10.scm
@@ -1,6 +1,8 @@
 ;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
 ;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
 
+(require-extension check)
+
 (define (A x y)
   (cond ((= y 0) 0)
         ((= x 0) (* 2 y))
@@ -8,8 +10,10 @@
         (else (A (- x 1)
                  (A x (- y 1))))))
 
-(A 2 1)
-(A 2 2)
-(A 2 3)
-(A 2 4)
-(A 2 5)
+(check-reset!)
+(check (A 2 1) => 2)
+(check (A 2 2) => 4)
+(check (A 2 3) => 16)
+(check (A 2 4) => 65536)
+(check (A 2 5) => +inf)
+(check-report)
