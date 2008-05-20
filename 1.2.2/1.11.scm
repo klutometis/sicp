105dc1e29cb30b9fef4a3704f639c56d10d08f4b
Peter Danenberg <pcd@wikitex.org>
2008-05-20 08:15:33 -0700

    check n-iter, pascal

--- 1.2.2/1.11.scm
+++ 1.2.2/1.11.scm
@@ -1,7 +1,7 @@
 ;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
 ;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
 
-(load "../test/test-context.scm")
+(require-extension check)
 
 (define (f-rec n)
   (cond ((< n 3) n)
@@ -18,7 +18,5 @@
       (else (iter y z (+ (* 3 x) (* 2 y) z) (- count 1)))))
   (iter 0 1 2 n))
 
-(define n 5)
-(define n-rec (f-rec 5))
-(define n-iter (f-iter 5))
-(test "recursive vs. iterated f" n-rec n-iter '= =)
+(check (f-rec 5) => 25)
+(check (f-iter 5) => 25)
--- 1.2.2/1.12.scm
+++ 1.2.2/1.12.scm
@@ -1,14 +1,18 @@
 ;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
 ;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
 
+(require-extension check)
+
 ;; Actually: we just want an individual element, e.g.: (0, 0), (2, 1).
 (define (pascal n k)
   (cond ((zero? k) 1)
         ((= n k) 1)
         (else (+ (pascal (- n 1) (- k 1)) (pascal (- n 1) k)))))
 
-(pascal 4 0)
-(pascal 4 1)
-(pascal 4 2)
-(pascal 4 3)
-(pascal 4 4)
+(check-reset!)
+(check (pascal 4 0) => 1)
+(check (pascal 4 1) => 4)
+(check (pascal 4 2) => 6)
+(check (pascal 4 3) => 4)
+(check (pascal 4 4) => 1)
+(check-report)
