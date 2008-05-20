4fcf343217a307dccf52604dfd8bbf38dd2a9431
Peter Danenberg <pcd@wikitex.org>
2008-05-20 08:36:39 -0700

    test; superfluous files

--- 1.2.5/1.20.scm
+++ 1.2.5/1.20.scm
@@ -1,9 +1,17 @@
 ;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
 ;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
 
-(load "gcd.scm")
+(require-extension check)
 
-(trace-entry remainder)
+(define (remainder a b)
+  (if (< a b)
+      a
+      (remainder (- a b) b)))
+
+(define (gcd a b)
+  (if (zero? b)
+      a
+      (gcd b (remainder a b))))
 
 ;; Seems to work for this example, but the theory is shaky:
 ;; Build up opaque metavariables; and add their degree.
@@ -12,6 +20,7 @@
 (define beta (remainder 40 alpha))
 (define gamma (remainder alpha beta))
 (define delta (remainder beta gamma))
+
 ;; The degree of delta is seven; gamma, four.  The evaluation
 ;; of delta results in eighteen remainders (the answer).
-(gcd gamma delta)
+(check (gcd gamma delta) => 2)
--- 1.2.5/remainder.scm
+++
@@ -1,7 +0,0 @@
-;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
-;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
-
-(define (remainder a b)
-  (if (< a b)
-      a
-      (remainder (- a b) b)))
