8c2afbed81e378f624ea8f7183eba28658544549
Peter Danenberg <pcd@wikitex.org>
2008-05-20 06:26:39 -0700

    78ify 1.2

--- 1.1.6/1.2.scm
+++ 1.1.6/1.2.scm
@@ -1,15 +1,11 @@
 ;;; Solutions copyright (C) 2007, Peter Danenberg; http://wizardbook.org
 ;;; Source code copyright (C) 1996, MIT; http://mitpress.mit.edu/sicp
 
-(load "test-context.scm")
+(require-extension check)
 
 (define quotient
   (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
      (* 3 (- 6 2) (- 2 7))))
 
 
-(test '(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
-          (* 3 (- 6 2) (- 2 7)))
-      (/ -37 150)
-      quotient
-      '= =)
+(check quotient => (/ -37 150))
