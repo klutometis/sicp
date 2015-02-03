1c1c6dc1215b06af219fbffa2fec3d0edfc6c3ae
Peter Danenberg <pcd@roxygen.org>
2015-02-02 19:21:12 -0600

    Standardize on the simple average.

--- 3.75.scm
+++ 3.75.scm
@@ -13,37 +13,19 @@
   (list->stream '(1 2 1.5 1 0.5 -0.1 -2 -3 -2 -0.5 0.2 3 4)))
 
 ;;; Using simple moving average
-(define (make-zero-crossings-simple input-stream last-value last-last-value)
+(define (make-zero-crossings input-stream last-value last-last-value)
   (if (stream-null? input-stream)
       stream-null
       (let ((avpt (+ last-value (/ (- (stream-car input-stream) last-last-value) 2))))
         (cons-stream
          (sign-change-detector last-value avpt)
-         (make-zero-crossings-simple
+         (make-zero-crossings
           (stream-cdr input-stream)
           avpt
           last-value)))))
 
-;;; Using cumulative moving average.
-(define (make-zero-crossings-cumulative input-stream last-value n)
-  (if (stream-null? input-stream)
-      stream-null
-      (let ((avpt (/ (+ (stream-car input-stream) (* n last-value)) (+ n 1))))
-        (cons-stream
-         (sign-change-detector last-value avpt)
-         (make-zero-crossings-cumulative
-          (stream-cdr input-stream)
-          avpt
-          (+ n 1))))))
+(define zero-crossings
+  (make-zero-crossings sense-data 0 0))
 
-(define zero-crossings-simple
-  (make-zero-crossings-simple (stream-cdr sense-data) 0 0))
-
-(define zero-crossings-cumulative
-  (make-zero-crossings-cumulative (stream-cdr sense-data) 0 0))
-
-(test '(0 0 0 0 0 -1 0 0 0 0 1 0)
-      (stream->list zero-crossings-simple))
-
-(test '(0 0 0 0 0 0 -1 0 0 0 1 0)
-      (stream->list zero-crossings-cumulative))
+(test '(0 0 0 0 0 0 -1 0 0 0 0 1 0)
+      (stream->list zero-crossings))
